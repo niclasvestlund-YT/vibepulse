669f3d3e9f971139cd54aa9958911a94e2de0e77
Niclas Vestlund <niclas.vestlund@gmail.com>
2026-09-20 01:32:02 +0200

    Align startup health fixtures with updated host

--- test/test_vibepulse_codex_plugin.py
+++ test/test_vibepulse_codex_plugin.py
@@ -30,7 +30,7 @@ from types import SimpleNamespace
 ROOT = Path(__file__).resolve().parents[1]
 SCRIPTS = ROOT / ".agents/plugins/plugins/vibepulse/scripts"
 MAX_HOOK_INPUT = 64 * 1024
-HOST_SOURCE_FINGERPRINT = "e0e425fb9e6b"
+HOST_SOURCE_FINGERPRINT = "81244dda23e8"
 
 PERMISSION = {
     "hook_event_name": "PermissionRequest",
